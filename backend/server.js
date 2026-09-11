c540e997843c12e36728d145423969124a0e4bfa
Sri Harini <sharini479@gmail.com>
2026-09-11 12:11:01 +0530

    Added rate-limiting

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const connectDB = require('./config/db');
 connectDB();
+const limiter = rateLimit({
+    windowMs : 15*60*1000,
+    max:100,
+    message: "Too many requests"
+});
 const port = process.env.PORT||5000;
 const {errorHandler} = require('./middleware/errorMiddleware');
 const app = express();
+app.use(limiter);
 app.use(cors({
     origin:'http://localhost:5173'
 }));
